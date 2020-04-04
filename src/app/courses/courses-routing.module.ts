2f90a36a9c8ab4fec59b54510721d98d6d95013c
marcmedhat6211 <marcmedhat6211@gmail.com>
2020-04-04 23:39:27 +0200

    showing courses details

--- src/app/courses/courses-routing.module.ts
+++ src/app/courses/courses-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { CoursesComponent } from './courses.component';
+import { CourseDetailsComponent } from './course-details/course-details.component';
 
-const routes: Routes = [{ path: '', component: CoursesComponent }];
+const routes: Routes = [{ path: '', component: CoursesComponent, 
+                      children: [{path: ':id', component: CourseDetailsComponent}]
+                    }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
